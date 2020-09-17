1fa67a5783f52669b9037390638c6fb65b118c33
Conqu3red <Conqu3red@users.noreply.github.com>
2020-09-17 20:22:26 +0100

    Fix returned time since reload

--- CacheManager.py
+++ CacheManager.py
@@ -8,15 +8,15 @@ weekly_url = "https://dfp529wcvahka.cloudfront.net/manifests/weeklyChallenges.js
 download_challenges_url = "https://dfp529wcvahka.cloudfront.net/manifests/leaderboards/challenges/scores/{0}.json"
 identifiers = {
 "1":  ["mAp2V","NAgrb","Bbm2A","0A5Zn","JbOmn","aVeaV","5VlRA","gnR7V","7b7xA","WAGoA","ObqMb","EAaRn","Xb3Ob","1nXeV","EABGn","6Vw5A"], #World 1
-"2":  ["zA0Mn","kb2wA","gb1Kn","AoQzv","MAr3n","QVYRb","JnZ2n","PV4Qb","yb8Pb","gnyrV","MAEoV","qn9JV","5AWzb","MV6DA","1nQen","bmw2n"], #World 2
-"3":  ["jnL9V","vAMDb","lnKkn","bdjrn","JbDPb","mnkRA","Ap22V","abx5A","5VJBV","zAvPV","JVPon","ZbjWA","XVzGb","A5QZA","ObNoA","AgXrn"], #World 3
+"2":  ["zA0Mn","kb2wA","gb1Kn","AoQzv","MAr3n","QVYRb","JnZ2n","PV4Qb","yb8Pb","gnyrV","MAEoV","qn9JV","lnKkn","MV6DA","1nQen","bmw2n"], #World 2
+"3":  ["jnL9V","vAMDb","5AWzb","bdjrn","JbDPb","mnkRA","Ap22V","abx5A","5VJBV","zAvPV","JVPon","ZbjWA","XVzGb","A5QZA","ObNoA","AgXrn"], #World 3
 "4":  ["nk2rA","nKv3A","bje7V","b2qZV","VPllV","nyl7A","VY39A","Av89V","bxeQA","AW4kn","bDDrb","nQlOA","nZLOb","n9ZBV","AM8vb","V6BNV"], #World 4
-"5":  ["AGDKo","nRM67","AoqEe","bqYEM","b7l7x","Aa58R","b3WOO","VwMk5","AB1QG","nXZMe","A0ZgM","AEOYo","b1WmK","nKvxk","VY3MR","nZLM2"], #World 5
+"5":  ["Aa58R","b7l7x","AB1QG","nZLM2","nRM67","b1WmK","AoqEe","b3WOO","AGDKo","nKvxk","nXZMe","A0ZgM","AEOYo","VwMk5","bqYEM","VY3MR"], #World 5
 "1c": ["AW7zA","bq7Mn","Aa7RV","Ao2eb","nXLen","Vw85b","V6qDn","nK8kV","b33Ob","A0rMV","nZ32V","VY4Rn","ABDGV","AEBon","b1qKb","n9dJA"], #World 1c
-"2c": ["VzoGb","nQOeb","b2WwA","b1p95","AMmDA","VJDBA","AvMPb","bN5oV","bDzPn","bxN5n","nklRV","VPgon","b83PV","bjMWV","nLm9V","V4LQV"], #World 2c
-"3c": ["Vezab","Arl3V","bdarA","A5GZV","bqYMb","AGDon","ApQ9n","ApQ2n","AgrJb","bdaqA","Agrrb","b7lxA","bmK2n","Vl5Rb","bOpmA","nRM7A"], #World 3c
+"2c": ["VzoGb","nQOeb","b2WwA","b1p95","AMmDA","VJDBA","AvMPb","bN5oV","bDzPn","bxN5n","nklRV","VPgon","bdarA","bjMWV","nLm9V","V4LQV"], #World 2c
+"3c": ["Vezab","Arl3V","b83PV","A5GZV","bqYMb","AGDon","ApQ9n","ApQ2n","AgrJb","bdaqA","Agrrb","b7lxA","bmK2n","Vl5Rb","bOpmA","nRM7A"], #World 3c
 "4c": ["b7ljA","bOpaA","Aa5OA","VwMpA","A0ZaV","bqYob","A5GJV","b3WDb","nXZzb","nRM8A","AGD6n","AB19b","Vez1b","bmK9n","AoqpV","Vl5gb"], #World 4c
-"5c": ["VJGKB","AgrJr","ApQj2","V6BRD","nLar9","bx3E5","VzJ2G","nQm4e","bdaJr","Av8NP","bNdLo","b2Z5w","ArdM3","V45eQ","VPeKo","b8zGP"], #World 5c
+"5c": ["bx3E5","nLar9","bdaJr","b8zGP","AgrJr","ArdM3","ApQj2","VzJ2G","VJGKB","V45eQ","Av8NP","bNdLo","b2Z5w","nQm4e","V6BRD","VPeKo"], #World 5c
 "6":  ["bOeMR","A5XOx","nR5Re","bm2OL","b7WRR","Vl2Wp","VeDY5","AGvLD","AaE79","bqe7e","b3Y34","nXvLa","ABND7","Vwa8y","A0QrO","Aor26"]	 #World 6
 }
 
